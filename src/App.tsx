53aaead3977317fcd204d025d41ae5dabdd3950b
CyrusWeir <cyrusweir@gmail.com>
2024-10-23 10:02:18 +0200

    added mew to the array, and made display dynamic according to availability of image src

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <div>
       <PokemonCard />
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,7 +1,18 @@
 function PokemonCard() {
-  return <figure>
-<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur"></img>
-  <figcaption>Bulbasaur</figcaption></figure>;
-}
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
-export default PokemonCard;
+  const pokemon = pokemonList[1]
+  return pokemon.imgSrc !== undefined ? <><img src={pokemon.imgSrc} alt={pokemon.name}></img><figcaption>{pokemon.name}</figcaption></> :
+    <><p>???</p> <figcaption>{pokemon.name}</figcaption> </>
+
+ 
+} export default PokemonCard;
