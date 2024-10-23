077ee32a0679f6daa5a9535d7f3c08b3c1aa0c10
CyrusWeir <cyrusweir@gmail.com>
2024-10-23 11:22:33 +0200

    added the props and reconfigured the card and app page

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,16 +1,7 @@
-function PokemonCard() {
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+function PokemonCard({pokemon}) {
+  console.log(pokemon)
 
-  const pokemon = pokemonList[1]
+
   return pokemon.imgSrc !== undefined ? <><img src={pokemon.imgSrc} alt={pokemon.name}></img><figcaption>{pokemon.name}</figcaption></> :
     <><p>???</p> <figcaption>{pokemon.name}</figcaption> </>
 
